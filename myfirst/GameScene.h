05ded9cb5460bbdde5061630cacf0e14466d6b0c
sheen <sheen@sheendemac.local>
2017-08-01 16:03:58 +0800

    Initial Commit

---
+++ myfirst/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  myfirst
+//
+//  Created by sheen on 2017/8/1.
+//  Copyright © 2017年 sheen. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ myfirst/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  myfirst
+//
+//  Created by sheen on 2017/8/1.
+//  Copyright © 2017年 sheen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <GameplayKit/GameplayKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
